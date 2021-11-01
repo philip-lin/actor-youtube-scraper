be2c5adcfa31f62fb8062c1b1e35e640852e8571
Philip Lin <pl1324@gmail.com>
2021-11-01 10:55:09 -0400

    Update utility.js

--- src/utility.js
+++ src/utility.js
@@ -589,30 +589,30 @@ module.exports.proxyConfiguration = async ({
 }) => {
     const configuration = await Apify.createProxyConfiguration(proxyConfig);
 
-    // this works for custom proxyUrls
-    if (Apify.isAtHome() && required) {
-        if (!configuration
-            || (!configuration.usesApifyProxy
-                && (!configuration.proxyUrls || !configuration.proxyUrls.length)) || !configuration.newUrl()) {
-            throw '\n=======\nWrong Input! You must use Apify proxy or custom proxies with this scraper!\n\n=======';
-        }
-    }
+//     // this works for custom proxyUrls
+//     if (Apify.isAtHome() && required) {
+//         if (!configuration
+//             || (!configuration.usesApifyProxy
+//                 && (!configuration.proxyUrls || !configuration.proxyUrls.length)) || !configuration.newUrl()) {
+//             throw '\n=======\nWrong Input! You must use Apify proxy or custom proxies with this scraper!\n\n=======';
+//         }
+//     }
 
-    // check when running on the platform by default
-    if (force) {
-        // only when actually using Apify proxy it needs to be checked for the groups
-        if (configuration && configuration.usesApifyProxy) {
-            if (blacklist.some((blacklisted) => (configuration.groups || []).includes(blacklisted))) {
-                throw '\n=======\nThese proxy groups cannot be used in this actor.'
-                    + `Choose other group or contact support@apify.com to give you proxy trial:\n\n*  ${blacklist.join('\n*  ')}\n\n=======`;
-            }
+//     // check when running on the platform by default
+//     if (force) {
+//         // only when actually using Apify proxy it needs to be checked for the groups
+//         if (configuration && configuration.usesApifyProxy) {
+//             if (blacklist.some((blacklisted) => (configuration.groups || []).includes(blacklisted))) {
+//                 throw '\n=======\nThese proxy groups cannot be used in this actor.'
+//                     + `Choose other group or contact support@apify.com to give you proxy trial:\n\n*  ${blacklist.join('\n*  ')}\n\n=======`;
+//             }
 
-            // specific non-automatic proxy groups like RESIDENTIAL, not an error, just a hint
-            if (hint.length && !hint.some((group) => (configuration.groups || []).includes(group))) {
-                Apify.utils.log.info(`\n=======\nYou can pick specific proxy groups for better experience:\n\n*  ${hint.join('\n*  ')}\n\n=======`);
-            }
-        }
-    }
+//             // specific non-automatic proxy groups like RESIDENTIAL, not an error, just a hint
+//             if (hint.length && !hint.some((group) => (configuration.groups || []).includes(group))) {
+//                 Apify.utils.log.info(`\n=======\nYou can pick specific proxy groups for better experience:\n\n*  ${hint.join('\n*  ')}\n\n=======`);
+//             }
+//         }
+//     }
 
     return configuration;
 };
